ebb41507c0cdfa1b71751d80249d80404d0c2da5
elliot ellis <elliotellis@me.com>
2026-04-30 13:37:12 +0200

    Missing images error fix attempt

--- site/snippets/projects/all.php
+++ site/snippets/projects/all.php
@@ -4,7 +4,9 @@
       <?php if ($project->works()): ?>
         <?php foreach ($project->content()->works()->toStructure() as $work): ?>
           <figure class="project-image piw-<?php $work->width(); ?>">
-            <img src="<?php print($work->image()->toFile()->url()); ?>" alt="<?php print($work->title()); ?>">
+            <?php if ($work->image()): ?>
+              <img src="<?php print($work->image()->toFile()->url()); ?>" alt="<?php print($work->title()); ?>">
+            <?php endif; ?>
             <?php if ($work->text()): ?>
               <figcaption><?php print($work->caption()->kt()); ?></figcaption>
             <?php endif; ?>
